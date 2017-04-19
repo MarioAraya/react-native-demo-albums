20397fb13df75a201fa88f147fb4dde50651858c
Mario Araya Romero <arayaromero@gmail.com>
2017-04-19 13:24:03 -0300

    state added + re-render after fetch data (setState method)

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -3,12 +3,16 @@ import { Text, View } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+    // this is a "class-level property"
+    state = { albums: [] };
+
     componentWillMount() {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-        .then(res => console.log(res));
+        .then(res => this.setState({ albums: res.data }));
     }
 
     render() {
+        console.log(this.state);
         return (
             <View>
                 <Text>Album list!!!</Text>
